2f68df82c988136c69ef28a75f4d1f9dec8fb49f
Fedor Shamakhov <fedor.shamakhov@gmail.com>
2020-06-17 19:09:33 +0800

    initial

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
+Implementation of custom hooks for friends list online status.
+
 ## Available Scripts
 
 In the project directory, you can run:
--- src/App.js
+++ src/App.js
@@ -1,26 +1,121 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const friendList = [
+  { id: 1, name: 'Phoebe' },
+  { id: 2, name: 'Rachel' },
+  { id: 3, name: 'Ross' },
+];
+
+const ChatAPIFunction = () => {
+  let friends = [];
+
+  return {
+    subscribeToFriendStatus: (friendID, handleStatusChange) => {
+      const existingFriend = friends.find(f => f.id === friendID);
+      let online;
+      if (!existingFriend) {
+        online = Math.random() > 0.5;
+        const name = friendList.find(f => f.id === friendID).name;
+        friends.push({
+          id: friendID,
+          statusChange: handleStatusChange,
+          subscribed: true,
+          name,
+          online,
+        });
+      } else {
+        online = existingFriend.online;
+        existingFriend.subscribed = true;
+      }
+      handleStatusChange({ isOnline: online });
+    },
+    unsubscribeFromFriendStatus: (friendID, handleStatusChange) => {
+      const friend = friends.find(f => f.id === friendID);
+      friend.subscribed = false;
+    },
+    changeStatuses: () => {
+      for (let friend of friends) {
+        let online = true;
+        if (Math.random() > 0.5) {
+          online = false;
+        }
+        friend.online = online;
+        if (friend.subscribed) friend.statusChange({ isOnline: online });
+      }
+    },
+    printFriends: () => {
+      return JSON.stringify(
+        friends.map( f => ({ name: f.name, online: f.online })),
+        undefined,
+        2
+      );
+    }
+  };
+};
+
+const ChatAPI = ChatAPIFunction();
+
+function useFriendStatus(friendID) {
+  const [isOnline, setIsOnline] = useState(null);
+
+  useEffect(() => {
+    function handleStatusChange(status) {
+      setIsOnline(status.isOnline);
+    }
+
+    const name = friendList.find(f => f.id === friendID).name;
+    ChatAPI.subscribeToFriendStatus(friendID, handleStatusChange);
+    return () => {
+      ChatAPI.unsubscribeFromFriendStatus(friendID, handleStatusChange);
+    };
+  });
+
+  return isOnline;
 }
 
-export default App;
+function Circle(props) {
+  return (
+    <svg height="20" width="20">
+      <circle cx="10" cy="10" r="8" stroke="black" strokeWidth="1" fill={props.color} />
+      Sorry, your browser does not support inline SVG.  
+    </svg> 
+  )
+}
+
+export default function ChatRecipientPicker() {
+  const [recipientID, setRecipientID] = useState(1);
+  const [friendsJSON, setFriendsJSON] = useState('');
+  const isRecipientOnline = useFriendStatus(recipientID);
+
+  useEffect(() => {
+    setFriendsJSON(ChatAPI.printFriends());
+  });
+
+  return (
+    <>
+    <div style={{display: 'inline-flex'}}>
+      <Circle color={isRecipientOnline ? 'green' : 'red'} />
+      <select
+        value={recipientID}
+        onChange={e => setRecipientID(Number(e.target.value))}
+      >
+        {friendList.map(friend => (
+          <option key={friend.id} value={friend.id}>
+            {friend.name}
+          </option>
+        ))}
+      </select>
+      <button 
+        style={{marginLeft: '5px'}}
+        onClick={ChatAPI.changeStatuses}>
+          Change Statuses
+      </button>
+    </div>
+    <div>
+      <pre>
+        {friendsJSON}
+      </pre>
+    </div>
+    </>
+  );
+}
